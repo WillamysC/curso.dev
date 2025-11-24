fa3efcb38d3b42a614321be0e80f9ae8b31868f7
Willamys <98623054+WillamysC@users.noreply.github.com>
2025-11-24 22:06:29 +0000

    fix prettier config

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,8 @@ function Home() {
   return <h1>Te amo, Mary Ingrid ❤️. Se tu me ama, dá uma risadinha! 😎 </h1>;
 }
 
+function teste2() {
+  console.log("indentação errada");
+}
+
 export default Home;
